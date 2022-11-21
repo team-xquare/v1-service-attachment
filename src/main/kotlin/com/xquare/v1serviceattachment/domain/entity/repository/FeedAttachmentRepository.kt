180b69c712ebee1b5a77ba08f9207adb5ed49faf
lyutvs <lyutvs@gmail.com>
2022-11-21 15:10:39 +0900

    📑 :: FeedAttachmentRepository

---
+++ src/main/kotlin/com/xquare/v1serviceattachment/domain/entity/repository/FeedAttachmentRepository.kt
@@ -0,0 +1,8 @@
+package com.xquare.v1serviceattachment.domain.entity.repository
+
+import com.xquare.v1serviceattachment.domain.entity.FeedAttachment
+import org.springframework.data.repository.CrudRepository
+import java.util.UUID
+
+interface FeedAttachmentRepository : CrudRepository<FeedAttachment, UUID> {
+}
