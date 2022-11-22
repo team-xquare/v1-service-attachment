53b29f4e76bae6879e844de2e614eb48bc880a2d
lyutvs <lyutvs@gmail.com>
2022-11-22 10:38:11 +0900

    ♻️ :: 코드리뷰 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
     implementation(Dependencies.AWS_S3)
     implementation(Dependencies.SENTRY)
     implementation(Dependencies.CLOUD_CONFIG)
-    implementation(Dependencies.UUID_V4)
+    implementation(Dependencies.UUID_V6)
 }
 
 dependencyManagement {
