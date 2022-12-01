657a902b8f833bee2595e678fb113617d188e259
lyutvs <lyutvs@gmail.com>
2022-12-01 21:17:40 +0900

    ♻️ :: 불필요한 의존성 삭제

--- buildSrc/src/main/kotlin/DependencyVersions.kt
+++ buildSrc/src/main/kotlin/DependencyVersions.kt
@@ -1,13 +1,6 @@
 object DependencyVersions {
     const val KTLINT_VERSION = "0.44.0"
     const val SPRING_CLOUD_VERSION = "2021.0.1"
-    const val MUTINY_VERSION = "1.4.0"
-    const val JDSL_VERSION = "2.0.1.RELEASE"
-    const val COROUTINE_VERSION = "1.6.1"
-    const val RABBIT_VERSION = "2.4.3"
-    const val MOCKK_VERSION = "1.12.3"
-    const val SPRING_MOCKK_VERSION = "3.1.1"
-    const val VAULT_VERSION = "3.1.1"
     const val SENTRY_VERSION = "6.5.0"
     const val AWS_S3 = "2.2.6.RELEASE"
 }
