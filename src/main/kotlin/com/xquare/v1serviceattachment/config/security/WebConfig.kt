e8b17b9ab816d6802ee428f97b71d9756e5037e7
lyutvs <lyutvs@gmail.com>
2022-12-05 17:08:51 +0900

    📑 :: WebConfig

---
+++ src/main/kotlin/com/xquare/v1serviceattachment/config/security/WebConfig.kt
@@ -0,0 +1,17 @@
+package com.xquare.v1serviceattachment.config.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry
+            .addMapping("/**")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowedOrigins("https://service.xquare.app/", "http://localhost:3000", "http://localhost:3001");
+    }
+}
