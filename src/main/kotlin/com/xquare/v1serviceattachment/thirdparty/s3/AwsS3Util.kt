5e2daf5afe547e1f4a577443a4b9797ec47a140b
lyutvs <lyutvs@gmail.com>
2022-12-05 19:16:35 +0900

    📑 :: AwsS3Util

---
+++ src/main/kotlin/com/xquare/v1serviceattachment/thirdparty/s3/AwsS3Util.kt
@@ -0,0 +1,50 @@
+package com.xquare.v1serviceattachment.thirdparty.s3
+
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.internal.Mimetypes
+import com.amazonaws.services.s3.model.CannedAccessControlList
+import com.amazonaws.services.s3.model.ObjectMetadata
+import com.amazonaws.services.s3.model.PutObjectRequest
+import com.xquare.v1serviceattachment.attachment.exception.FileIOInterruptedException
+import com.xquare.v1serviceattachment.attachment.properties.AwsS3Properties
+import org.springframework.stereotype.Component
+import java.io.File
+import java.io.IOException
+
+@Component
+class AwsS3Util(
+    private val awsS3Properties: AwsS3Properties,
+    private val amazonS3Client: AmazonS3Client
+) {
+
+    fun upload(file: File): String {
+        inputS3(file, file.name)
+
+        return getResource(file.name)
+    }
+
+    private fun inputS3(file: File, fileName: String) {
+        try {
+            val inputStream = file.inputStream()
+            val objectMetadata = ObjectMetadata().apply {
+                this.contentLength = file.length()
+                this.contentType = Mimetypes.getInstance().getMimetype(file)
+            }
+
+            amazonS3Client.putObject(
+                PutObjectRequest(awsS3Properties.bucket, fileName, inputStream, objectMetadata)
+                    .withCannedAcl(
+                        CannedAccessControlList.PublicRead
+                    )
+            )
+
+            file.delete()
+        } catch (e: IOException) {
+            throw FileIOInterruptedException
+        }
+    }
+
+    private fun getResource(fileName: String): String {
+        return amazonS3Client.getResourceUrl(awsS3Properties.bucket, fileName)
+    }
+}
