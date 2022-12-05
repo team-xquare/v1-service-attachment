4ac9e653d6725b3cd6c412502c284f450b112684
lyutvs <lyutvs@gmail.com>
2022-12-05 17:07:38 +0900

    📑 :: AttachmentException

---
+++ src/main/kotlin/com/xquare/v1serviceattachment/config/error/AttachmentException.kt
@@ -0,0 +1,7 @@
+package com.xquare.v1serviceattachment.config.error
+
+abstract class AttachmentException(
+    val errorCode: ErrorCode
+): RuntimeException() {
+    override fun fillInStackTrace() = this
+}
