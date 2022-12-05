a9578e371a9bb78f12b96bf7349857a266ff63c1
lyutvs <lyutvs@gmail.com>
2022-12-05 23:07:08 +0900

    📑  :: AttachmentFacade

---
+++ src/main/kotlin/com/xquare/v1serviceattachment/attachment/facade/AttachmentFacade.kt
@@ -0,0 +1,24 @@
+package com.xquare.v1serviceattachment.attachment.facade
+
+import org.springframework.stereotype.Component
+import org.springframework.web.multipart.MultipartFile
+import java.io.File
+import java.io.FileOutputStream
+import java.util.*
+
+@Component
+class AttachmentFacade {
+
+    fun transferFile(): (MultipartFile) -> File {
+        val transferFile = { multipartFile: MultipartFile ->
+            File("${UUID.randomUUID()}@${multipartFile.originalFilename}").let {
+                FileOutputStream(it).run {
+                    this.write(multipartFile.bytes)
+                    this.close()
+                }
+                it
+            }
+        }
+        return transferFile
+    }
+}
