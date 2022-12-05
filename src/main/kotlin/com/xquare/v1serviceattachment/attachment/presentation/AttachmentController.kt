7eca6dd69492bb12131e54e75c92f2e893c21af7
lyutvs <lyutvs@gmail.com>
2022-12-05 19:16:07 +0900

    📑 :: AttachmentController

---
+++ src/main/kotlin/com/xquare/v1serviceattachment/attachment/presentation/AttachmentController.kt
@@ -0,0 +1,19 @@
+package com.xquare.v1serviceattachment.attachment.presentation
+
+import com.xquare.v1serviceattachment.attachment.service.ImageUploadService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.io.File
+
+@RestController
+@RequestMapping("/attachment")
+class AttachmentController(
+    private val imageUploadService: ImageUploadService
+) {
+
+    @PostMapping
+    fun imageUpload(file: File) {
+        imageUploadService.execute(file);
+    }
+}
