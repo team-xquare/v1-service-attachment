eb5b2f7ef916391f9ae30d5d6f1354960257c574
lyutvs <lyutvs@gmail.com>
2022-12-07 23:13:58 +0900

    ⚰️  :: 필요없는 클래스 삭제

--- src/main/kotlin/com/xquare/v1serviceattachment/config/config/PropertiesScanConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.xquare.v1serviceattachment.config.config
-
-import com.xquare.v1serviceattachment.attachment.properties.AwsS3Properties
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
-import org.springframework.context.annotation.Configuration
-
-@ConfigurationPropertiesScan(basePackageClasses = [AwsS3Properties::class])
-@Configuration
-class PropertiesScanConfig
