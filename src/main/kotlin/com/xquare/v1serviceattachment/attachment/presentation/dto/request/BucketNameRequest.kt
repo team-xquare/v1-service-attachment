c0d9f64dc4ad3ba52b441d30229bf5639105deb9
lyutvs <lyutvs@gmail.com>
2022-12-08 19:00:10 +0900

    ♻️ :: ktlint

--- src/main/kotlin/com/xquare/v1serviceattachment/attachment/presentation/dto/request/BucketNameRequest.kt
+++ src/main/kotlin/com/xquare/v1serviceattachment/attachment/presentation/dto/request/BucketNameRequest.kt
@@ -2,7 +2,7 @@ package com.xquare.v1serviceattachment.attachment.presentation.dto.request
 
 import javax.validation.constraints.NotNull
 
-data class BucketNameRequest (
+data class BucketNameRequest(
 
     @NotNull
     val bucketName: String
