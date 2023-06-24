a1f422ca608af86aff5420a31b6d3d6e2197280d
poka <khs0826@dsm.hs.kr>
2023-06-24 13:41:54 +0900

    🐛 :: 개행문자 변경

--- src/main/kotlin/com/xquare/v1serviceattachment/attachment/exception/FileInvalidContentTypeException.kt
+++ src/main/kotlin/com/xquare/v1serviceattachment/attachment/exception/FileInvalidContentTypeException.kt
@@ -1,5 +1,4 @@
 package com.xquare.v1serviceattachment.attachment.exception
-
 import com.xquare.v1serviceattachment.config.error.AttachmentException
 import com.xquare.v1serviceattachment.config.error.ErrorCode
 
--- src/main/kotlin/com/xquare/v1serviceattachment/attachment/presentation/dto/response/PresignedUrlResponse.kt
+++ src/main/kotlin/com/xquare/v1serviceattachment/attachment/presentation/dto/response/PresignedUrlResponse.kt
@@ -1,5 +1,4 @@
 package com.xquare.v1serviceattachment.attachment.presentation.dto.response
-
 data class PresignedUrlResponse(
     val url: String,
     val presignedUrl: String,
