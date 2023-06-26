d43ddb2f470b84058bb9b2da31944eb8503a3878
poka <khs0826@dsm.hs.kr>
2023-06-26 12:50:14 +0900

    ♻️ :: PresignedUrlServingService로 이름 변경

--- src/main/kotlin/com/xquare/v1serviceattachment/attachment/presentation/AttachmentController.kt
+++ src/main/kotlin/com/xquare/v1serviceattachment/attachment/presentation/AttachmentController.kt
@@ -2,22 +2,19 @@ package com.xquare.v1serviceattachment.attachment.presentation
 
 import com.xquare.v1serviceattachment.attachment.presentation.dto.request.ImageFileRequest
 import com.xquare.v1serviceattachment.attachment.presentation.dto.response.PresignedUrlResponse
-import com.xquare.v1serviceattachment.attachment.presentation.dto.response.UploadFileResponse
-import com.xquare.v1serviceattachment.attachment.service.ImageUploadService
+import com.xquare.v1serviceattachment.attachment.service.PresignedUrlServingService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.multipart.MultipartFile
 
 @RestController
 @RequestMapping("/attachment")
 class AttachmentController(
-    private val imageUploadService: ImageUploadService
+    private val imageUploadService: PresignedUrlServingService
 ) {
 
     @ResponseStatus(HttpStatus.CREATED)
