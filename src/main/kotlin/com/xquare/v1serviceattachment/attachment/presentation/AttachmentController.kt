a39709e3f2923c462cea9275856d85a64228f62d
alsdl0629 <ehfehf0629@gmail.com>
2023-08-28 22:39:28 +0900

    🔀 :: 버그 수정

--- src/main/kotlin/com/xquare/v1serviceattachment/attachment/presentation/AttachmentController.kt
+++ src/main/kotlin/com/xquare/v1serviceattachment/attachment/presentation/AttachmentController.kt
@@ -1,13 +1,12 @@
 package com.xquare.v1serviceattachment.attachment.presentation
 
-import com.xquare.v1serviceattachment.attachment.presentation.dto.request.ImageFileRequest
+import com.xquare.v1serviceattachment.attachment.presentation.dto.request.FileRequest
 import com.xquare.v1serviceattachment.attachment.presentation.dto.response.PresignedUrlResponse
 import com.xquare.v1serviceattachment.attachment.service.PresignedUrlServingService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
@@ -20,7 +19,7 @@ class AttachmentController(
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    fun uploadPresigendURL(@RequestBody files: List<@Valid ImageFileRequest>): List<PresignedUrlResponse> {
-        return imageUploadService.execute(files)
+    fun uploadPresigendURL(@RequestBody @Valid files: FileRequest): List<PresignedUrlResponse> {
+        return imageUploadService.execute(files.imageFileRequests)
     }
 }
--- src/main/kotlin/com/xquare/v1serviceattachment/attachment/presentation/dto/request/ImageFileRequest.kt
+++ src/main/kotlin/com/xquare/v1serviceattachment/attachment/presentation/dto/request/ImageFileRequest.kt
@@ -1,15 +1,19 @@
 package com.xquare.v1serviceattachment.attachment.presentation.dto.request
 
-import com.xquare.v1serviceattachment.attachment.common.annotation.FileSizeLimit
+import javax.validation.Valid
 import javax.validation.constraints.Max
 import javax.validation.constraints.Pattern
-import javax.validation.constraints.Positive
+
+data class FileRequest(
+    @Valid
+    val imageFileRequests: List<ImageFileRequest>
+)
 
 data class ImageFileRequest(
     @field:Pattern(regexp = "^[A-Za-z0-9-_,\\s]+[.]{1}[A-Za-z]{3,4}\$", message = "올바른 파일명이 아닙니다")
     val originalFilename: String,
     @field:Pattern(regexp = "^\\s*([a-z]+)/([a-z0-9\\-+]+)\\s*(?:;(.*))?\$", message = "올바른 Content-Type이 아닙니다")
     val contentType: String,
-    @field:FileSizeLimit(value = 10)
+    @field:Max(10)
     val fileSize: Long,
 )
