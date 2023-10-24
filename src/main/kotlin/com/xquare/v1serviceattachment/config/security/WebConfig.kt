9925ccb4d2376a35e431b5104e7282c93e90e8c0
김은빈 <rlaisqls@gmail.com>
2023-10-24 16:12:52 +0900

    Update WebConfig.kt

--- src/main/kotlin/com/xquare/v1serviceattachment/config/security/WebConfig.kt
+++ src/main/kotlin/com/xquare/v1serviceattachment/config/security/WebConfig.kt
@@ -12,6 +12,19 @@ class WebConfig : WebMvcConfigurer {
             .addMapping("/**")
             .allowedMethods("*")
             .allowedHeaders("*")
-            .allowedOrigins("https://service.xquare.app/", "http://localhost:3000", "http://localhost:3001")
+            .allowedOrigins(
+                "https://prod-server.xquare.app/",
+                "https://stag-server.xquare.app/",
+                "https://service.xquare.app/",
+                "https://admin.xquare.app",
+                "https://admin.dsm-pick.com",
+                "https://teacher.dsm-pick.com",
+                "https://keeper.dsm-pick.com",
+                "https://admin.dsm-repo.com",
+                "https://user.dsm-repo.com",
+                "https://www.dsm-repo.com",
+                "http://localhost:3000",
+                "http://localhost:3001"
+            )
     }
 }
